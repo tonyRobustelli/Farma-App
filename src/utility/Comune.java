a04c8d633bb8a0263ff997d060d75524d1fc8311
Antonio Robustelli <tony.robustelli95@hotmail.com>
2020-02-20 22:34:47 +0100

    Update Comune.java

--- src/utility/Comune.java
+++ src/utility/Comune.java
@@ -1,1 +1,19 @@
+package utility;
 
+public class Comune 
+{
+	private String nome;
+	
+	public Comune(String nome) 
+	{
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
